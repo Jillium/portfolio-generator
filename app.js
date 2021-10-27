30e99ee0b259d5f18700fc5137d62cc2019f8805
Jillium <86424914+Jillium@users.noreply.github.com>
2021-10-27 14:44:35 -0600

    capture-input

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log("================");
+
+    // is the same as this
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
